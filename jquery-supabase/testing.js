74f9b5876359391255c10ff14b0f4cb3b10b3200
minty-E <96228127+minty-E@users.noreply.github.com>
2023-10-06 10:23:48 -0700

    update

--- jquery-supabase/testing.js
+++ jquery-supabase/testing.js
@@ -1,34 +1,44 @@
-import { corsHeaders } from './cors.js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-storage.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// Initialize Firebase
+
 
 $(document).ready(function() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyB2zCM_mj8Wj-XRFS3SBXAcNDDvHBX-SfI",
+        authDomain: "testing-jquery-8088f.firebaseapp.com",
+        projectId: "testing-jquery-8088f",
+        storageBucket: "testing-jquery-8088f.appspot.com",
+        messagingSenderId: "617765066780",
+        appId: "1:617765066780:web:41031b3c41e18a323267d9"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const storage = getStorage(app);
     $("#uploadButton").on('click', function() {
         console.log("clicked")
-        const audioInput = document.getElementById('audioInput');
-        const file = audioInput.files[0];
+        var audioInput = document.getElementById('audioInput');
+        var file = audioInput.files[0];
         if (file) {
-            const formData = new FormData();
-            formData.append('audio', file);
+            var storageRef = firebase.storage().ref();
+            var fileRef = storageRef.child('audio/' + file.name);
+            var uploadTask = fileRef.put(file);
 
-            $.ajax({
-                type: 'POST',
-                url: 'https://uzlnqvnuvuybwzbieomy.supabase.co/storage/v1/object/audioFiles/',
-                data: formData,
-                processData: false,
-                contentType: false,
-                crossDomain: true,
-                xhrFields: {
-                    withCredentials: true
-                },
-                beforeSend: function (xhr) {
-                    xhr.setRequestHeader('apikey', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6bG5xdm51dnV5Ynd6Ymllb215Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzkzMzE0NiwiZXhwIjoyMDA5NTA5MTQ2fQ.jeJAhv7fwHIufl3Oe0kpka_WJF8l4oZJV8ViAPNJUPI');
-                },
-                success: function (data) {
-                    console.log("audio uploaded", data);
-                },
-                error: function (data) {
-                    console.log("error uploading audio", data);
-                }
-
+            uploadTask.on('state_changed', function(snapshot){
+                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+            }, function(error) {
+                console.log("error uploading audio");
+            }, function() {
+                uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL){
+                    console.log('audio file available at', downloadURL);
+                });
             });
         } else {
             alert('Select an audio file to uploaded')
